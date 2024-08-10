d98a5be0604d0a5fa137dfffd376538e40f9a017
paiq <100478607+naufaiqr@users.noreply.github.com>
2024-08-10 10:05:17 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// hide navbar on scroll down
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+// active state button (it works only in phone mode)
+var header = document.getElementById("menu");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  if (current.length > 0) { 
+    current[0].className = current[0].className.replace(" active", "");
+  }
+  this.className += " active";
+  });
+}
